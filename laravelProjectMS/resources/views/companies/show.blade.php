16b3f192b6cbc2fbb4d1fd58f4b38229d05e0b8c
Donald Moore <donmoore26762671@gmail.com>
2017-12-18 00:28:00 -0500

    tweaked sidebar on the show page for individual companies

--- laravelProjectMS/resources/views/companies/show.blade.php
+++ laravelProjectMS/resources/views/companies/show.blade.php
@@ -32,13 +32,21 @@
 </div>
 
 <div class="col-sm-3 col-md-3 col-lg-3 pull-right">
-    <div class="sidebar-module sidebar-module-inset">
+    <!--<div class="sidebar-module sidebar-module-inset">
         <h4>About</h4>
         <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+    </div>-->
+     <div class="sidebar-module">
+        <h4>Actions</h4>
+        <ol class="list-unstyled">
+          <li><a href="#">Edit</a></li>
+          <li><a href="#">Delete</a></li>
+           <li><a href="#">Add new member</a></li>
+        </ol>
     </div>
 
     <div class="sidebar-module">
-        <h4>Archives</h4>
+        <h4>Members</h4>
         <ol class="list-unstyled">
           <li><a href="#">March 2014</a></li>
           <li><a href="#">February 2014</a></li>
@@ -55,14 +63,8 @@
         </ol>
     </div>
 
-    <div class="sidebar-module">
-        <h4>Elsewhere</h4>
-        <ol class="list-unstyled">
-          <li><a href="#">GitHub</a></li>
-          <li><a href="#">Twitter</a></li>
-          <li><a href="#">Facebook</a></li>
-        </ol>
-    </div>
+   
+
  </div>
                      
 
